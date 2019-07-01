a43ec4e1562c3fa7d1c4f04768c869481434f7c3
Konstantin Savonevich <kossav@icloud.com>
2019-07-01 19:32:29 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Savonevich Constantine on 7/1/19.
+//  Copyright © 2019 Savonevich Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
