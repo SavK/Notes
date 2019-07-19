1d03b16c58da725c4de039e1a762c6ec51be3914
Konstantin Savonevich <kossav@icloud.com>
2019-07-19 21:35:55 +0300

    load from FileManager

--- Notes/Model/FileNotebook.swift
+++ Notes/Model/FileNotebook.swift
@@ -10,13 +10,15 @@ import Foundation
 import CocoaLumberjack
 
 class FileNotebook {
-
+    
+    init(images: [Note]) {
+        self.notes = notes
+    }
     
     // MARK: - Stored Properties
     private(set) var notes: [Note]
-    let archiveURL: URL
     let documentDirectory = FileManager.default.urls(for: .cachesDirectory,
-                                                     in: .userDomainMask).first!
+                                                     in: .userDomainMask).first
     
     /// Add new note with UID checking
     public func add(_ note: Note) {
@@ -42,36 +44,48 @@ class FileNotebook {
     
     init(notes: [Note]) {
         self.notes = notes
-        archiveURL = documentDirectory.appendingPathComponent("FileNotebook").appendingPathExtension("json")
     }
     
     /// Save notes as JSON Data into file
     public func saveToFile() {
-        do {
-            let data = try JSONSerialization.data(withJSONObject: getJSONObject(), options: [])
-            try data.write(to: archiveURL)
-        } catch {
-            DDLogError("ERROR: \(error.localizedDescription)")
+        if let documentDirectory = documentDirectory {
+            do {
+                let jsonArray = notes.map { $0.json }
+                let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [])
+                let pathSave = documentDirectory.appendingPathComponent("notes.json")
+                FileManager.default.createFile(atPath: pathSave.path, contents: data, attributes: nil)
+            } catch let error {
+                DDLogError("ERROR: \(error.localizedDescription)")
+            }
         }
     }
     /// Load JSON Data from file and update notes
     public func loadFromFile() {
-        do {
-            let data = try Data(contentsOf: archiveURL, options: .mappedIfSafe)
-            let jsonDicts = try JSONSerialization.jsonObject(with: data,
-                                                             options: []) as! [[String: Any]]
+        notes.removeAll()
+        if let documentDirectory = documentDirectory {
+            do {
+                let pathFile = documentDirectory.appendingPathComponent("notes.json")
+                if let data = FileManager.default.contents(atPath: pathFile.path),
+                    let jsonData = try JSONSerialization.jsonObject(with: data,
+                                                                    options: []) as? [[String: Any]] {
             
-            notes.removeAll()
-            jsonDicts.forEach { notes.append(Note.parse(json: $0)!) }
-        } catch {
-            DDLogError("ERROR: \(error.localizedDescription)")
+                    for noteJson in jsonData {
+                        guard let note = Note.parse(json: noteJson) else { return }
+                        self.add(note)
+                    }
+                } else {
+                    DDLogError("Can't load notes from file.")
+                }
+            } catch {
+                DDLogError("ERROR: \(error.localizedDescription)")
+            }
         }
     }
     
     /// Convert notes into JSON Object
     private func getJSONObject() -> [[String:Any]] {
         var jsonArray = [[String: Any]]()
-
+        
         for note in notes {
             jsonArray.append(note.json)
         }
